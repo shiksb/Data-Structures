2dbf56f5c58420575622110eec5c676cda35878b
Shikhar Bakhda <ssb2189@columbia.edu>
2017-04-27 20:30:35 -0400

    started HW6

---
+++ HW6/Graph.java
@@ -0,0 +1,214 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Graph {
+
+  // Keep a fast index to nodes in the map
+  private Map<Integer, Vertex> vertexNames;
+
+  /**
+   * Construct an empty Graph with a map. The map's key is the name of a vertex
+   * and the map's value is the vertex object.
+   */
+  public Graph() {
+    vertexNames = new HashMap<>();
+  }
+
+  /**
+   * Adds a vertex to the graph. Throws IllegalArgumentException if two vertices
+   * with the same name are added.
+   * 
+   * @param v
+   *          (Vertex) vertex to be added to the graph
+   */
+  public void addVertex(Vertex v) {
+    if (vertexNames.containsKey(v.name))
+      throw new IllegalArgumentException("Cannot create new vertex with existing name.");
+    vertexNames.put(v.name, v);
+  }
+
+  /**
+   * Gets a collection of all the vertices in the graph
+   * 
+   * @return (Collection<Vertex>) collection of all the vertices in the graph
+   */
+  public Collection<Vertex> getVertices() {
+    return vertexNames.values();
+  }
+
+  /**
+   * Gets the vertex object with the given name
+   * 
+   * @param name
+   *          (String) name of the vertex object requested
+   * @return (Vertex) vertex object associated with the name
+   */
+  public Vertex getVertex(int name) {
+    return vertexNames.get(name);
+  }
+
+  /**
+   * Adds a directed edge from vertex u to vertex v
+   * 
+   * @param nameU
+   *          (String) name of vertex u
+   * @param nameV
+   *          (String) name of vertex v
+   * @param cost
+   *          (double) cost of the edge between vertex u and v
+   */
+  public void addEdge(int nameU, int nameV, Double cost) {
+    if (!vertexNames.containsKey(nameU))
+      throw new IllegalArgumentException(nameU + " does not exist. Cannot create edge.");
+    if (!vertexNames.containsKey(nameV))
+      throw new IllegalArgumentException(nameV + " does not exist. Cannot create edge.");
+    Vertex sourceVertex = vertexNames.get(nameU);
+    Vertex targetVertex = vertexNames.get(nameV);
+    Edge newEdge = new Edge(sourceVertex, targetVertex, cost);
+    sourceVertex.addEdge(newEdge);
+  }
+
+  /**
+   * Adds an undirected edge between vertex u and vertex v by adding a directed
+   * edge from u to v, then a directed edge from v to u
+   * 
+   * @param name
+   *          (String) name of vertex u
+   * @param name2
+   *          (String) name of vertex v
+   * @param cost
+   *          (double) cost of the edge between vertex u and v
+   */
+  public void addUndirectedEdge(int name, int name2, double cost) {
+    addEdge(name, name2, cost);
+    addEdge(name2, name, cost);
+  }
+
+
+  /**
+   * Computes the euclidean distance between two points as described by their
+   * coordinates
+   * 
+   * @param ux
+   *          (double) x coordinate of point u
+   * @param uy
+   *          (double) y coordinate of point u
+   * @param vx
+   *          (double) x coordinate of point v
+   * @param vy
+   *          (double) y coordinate of point v
+   * @return (double) distance between the two points
+   */
+  public double computeEuclideanDistance(double ux, double uy, double vx, double vy) {
+    return Math.sqrt(Math.pow(ux - vx, 2) + Math.pow(uy - vy, 2));
+  }
+
+  /**
+   * Computes euclidean distance between two vertices as described by their
+   * coordinates
+   * 
+   * @param u
+   *          (Vertex) vertex u
+   * @param v
+   *          (Vertex) vertex v
+   * @return (double) distance between two vertices
+   */
+  public double computeEuclideanDistance(Vertex u, Vertex v) {
+    return computeEuclideanDistance(u.x, u.y, v.x, v.y);
+  }
+
+  /**
+   * Calculates the euclidean distance for all edges in the map using the
+   * computeEuclideanCost method.
+   */
+  public void computeAllEuclideanDistances() {
+
+    for (Vertex u : getVertices())
+      for (Edge uv : u.adjacentEdges) {
+        Vertex v = uv.target;
+        uv.distance = computeEuclideanDistance(u.x, u.y, v.x, v.y);
+      }
+  }
+
+
+
+  // STUDENT CODE STARTS HERE
+
+  public void generateRandomVertices(int n) {
+    vertexNames = new HashMap<>(); // reset the vertex hashmap
+    Random rand = new Random();
+
+    for(int i = 0; i < n; i++){
+      int randIntX = (int) (rand.nextFloat()*100);
+      int randIntY = (int) (rand.nextFloat()*100);
+      vertexNames.put(i, new Vertex(i, randIntX, randIntY));
+      if(i != 0){
+        for(int j = 0; j < i; j++){
+          addEdge(j, i, 0.0);
+        }
+      }
+    }
+
+    computeAllEuclideanDistances(); // compute distances
+  }
+
+  public List<Edge> nearestNeighborTsp() {
+    int name = 0;
+    ArrayList<Edge> path = new ArrayList<>();
+    ArrayList<Vertex> toVisit = new ArrayList<>(getVertices());
+    toVisit.remove(getVertex(0));
+    while(toVisit.size() > 0){
+      Edge smallestEdge = findNearest(toVisit, getVertex(name));
+      path.add(smallestEdge);
+      name = smallestEdge.target.name;
+      toVisit.remove(smallestEdge.source);
+    }
+    System.out.println(path);
+    return path; // replace this line
+  }
+
+  private Edge findNearest(ArrayList<Vertex> vertices, Vertex u){
+      Edge nearest = new Edge(u, u, Double.MAX_VALUE);
+      for(Edge e : u.adjacentEdges){
+        if(vertices.contains(e.target) && e.distance < nearest.distance){
+          nearest = e;
+        }
+      }
+      return nearest;
+  }
+
+  public List<Edge> bruteForceTsp() {
+    return null; // replace this line
+  }
+
+  // STUDENT CODE ENDS HERE
+
+
+
+  /**
+   * Prints out the adjacency list of the graph for debugging
+   */
+  public void printAdjacencyList() {
+    for (int u : vertexNames.keySet()) {
+      StringBuilder sb = new StringBuilder();
+      sb.append(u);
+      sb.append(" -> [ ");
+      for (Edge e : vertexNames.get(u).adjacentEdges) {
+        sb.append(e.target.name);
+        sb.append("(");
+        sb.append(e.distance);
+        sb.append(") ");
+      }
+      sb.append("]");
+      System.out.println(sb.toString());
+    }
+  }
+}
